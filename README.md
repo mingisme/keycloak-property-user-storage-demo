92fb104371f2ef756f34fd518fd9230d0c4f247f
swang <mingisme@126.com>
2019-08-02 17:38:29 +0800

    configurable

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# keycloak-property-user-storage-demo
-external user storage demo with property file
+Remember to add blow element in standalone.xml
+
+            <spi name="storage">
+                <provider name="readonly-property-file" enabled="true">
+                    <properties>
+                        <property name="path" value="${jboss.server.config.dir}/example-users.properties"/>
+                    </properties>
+                </provider>
+            </spi>
--- src/main/java/com/swang/PropertyFileUserStorageProviderFactory.java
+++ src/main/java/com/swang/PropertyFileUserStorageProviderFactory.java
@@ -2,12 +2,20 @@ package com.swang;
 
 import org.apache.log4j.Logger;
 import org.keycloak.Config;
+import org.keycloak.common.util.EnvUtil;
 import org.keycloak.component.ComponentModel;
+import org.keycloak.component.ComponentValidationException;
 import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.RealmModel;
+import org.keycloak.provider.ProviderConfigProperty;
+import org.keycloak.provider.ProviderConfigurationBuilder;
 import org.keycloak.storage.UserStorageProviderFactory;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 import java.util.Properties;
 import java.util.stream.Stream;
 
@@ -18,32 +26,59 @@ public class PropertyFileUserStorageProviderFactory implements UserStorageProvid
 
     public static final String PROVIDER_NAME = "readonly-property-file";
 
+
     private static final Logger logger = Logger.getLogger(PropertyFileUserStorageProviderFactory.class);
+
+    protected static final List<ProviderConfigProperty> configMetadata;
+
+    static {
+        configMetadata = ProviderConfigurationBuilder.create()
+                .property().name("path")
+                .type(ProviderConfigProperty.STRING_TYPE)
+                .label("Path")
+                .defaultValue("${jboss.server.config.dir}/example-users.properties")
+                .helpText("File path to properties file")
+                .add().build();
+    }
+
     protected Properties properties = new Properties();
 
     @Override
-    public void init(Config.Scope config) {
-        InputStream is = getClass().getClassLoader().getResourceAsStream("/users.properties");
-
-        if (is == null) {
-            logger.warn("Could not find users.properties in classpath");
-        } else {
-            try {
-                properties.load(is);
-            } catch (IOException ex) {
-                logger.error("Failed to load users.properties file", ex);
-            }
-        }
+    public List<ProviderConfigProperty> getConfigProperties() {
+        return configMetadata;
     }
 
     @Override
     public PropertyFileUserStorageProvider create(KeycloakSession session, ComponentModel model) {
-        return new PropertyFileUserStorageProvider(session, model, properties);
+        String path = model.getConfig().getFirst("path");
+        path = EnvUtil.replace(path);
+        Properties props = new Properties();
+        try {
+            InputStream is = new FileInputStream(path);
+            props.load(is);
+            is.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return new PropertyFileUserStorageProvider(session, model, props);
     }
 
 
     @Override
     public String getId() {
         return PROVIDER_NAME;
+    }
+
+    @Override
+    public void validateConfiguration(KeycloakSession session, RealmModel realm, ComponentModel config)
+            throws ComponentValidationException {
+        String fp = config.getConfig().getFirst("path");
+        if (fp == null) throw new ComponentValidationException("user property file does not exist");
+        fp = EnvUtil.replace(fp);
+        File file = new File(fp);
+        if (!file.exists()) {
+            throw new ComponentValidationException("user property file does not exist");
+        }
     }
 }
